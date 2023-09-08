4ad6a3aa11abe1df0bda4c93ee4ee607aa32c1e2
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-09-08 17:26:28 -0500

    HomeScreen polish

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const meta = {
-  title: '',
-  meta: [],
-  link: [],
+  title: 'OGCovered - A better health for you',
+  meta: ['ogcovered', 'health insurance broker', 'health insurance', 'better health insurance', 'ogcovered health'],
+  link: ['https://www.ogcovered.com'],
   style: [],
   script: [],
 };
@@ -17,38 +17,34 @@ export default function Index() {
       </HelmetProvider>
       <>
         <section className='relative bg-white overflow-hidden' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center' }}>
-          <div className='bg-tarnsparent'>
+          <div className='bg-transparent'>
             <nav className='flex justify-between p-6 px-4'>
               <div className='flex justify-between items-center w-full'>
                 <div className='w-1/2 xl:w-1/3'>
-                  <a className='block max-w-max' href='#'>
-                    <img
-                      className='h-8'
-                      src='flex-ui-assets/logos/flex-ui-green-light.svg'
-                      alt=''
-                    />
+                  <a className='block max-w-max' href='/'>
+                    <img className='h-16' src='logo/ogcovered-logo.svg' alt='OGCovered, a better health for you' />
                   </a>
                 </div>
                 <div className='w-1/2 xl:w-1/3'>
                   <ul className='hidden xl:flex xl:justify-center'>
                     <li className='mr-12'>
-                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#'>
-                        Insurance Plans
+                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#mission'>
+                        Mision
                       </a>
                     </li>
                     <li className='mr-12'>
-                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#' >
+                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#becovered' >
+                        Coverage
+                      </a>
+                    </li>
+                    <li className='mr-12'>
+                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#benefits'>
                         Benefits
                       </a>
                     </li>
-                    <li className='mr-12'>
-                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#'>
-                        Information
-                      </a>
-                    </li>
                     <li>
-                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#'>
-                        Cost
+                      <a className='text-coolGray-500 hover:text-coolGray-900 font-medium' href='#qualify'>
+                        Do I qualify?
                       </a>
                     </li>
                   </ul>
@@ -64,11 +60,9 @@ export default function Index() {
               <button className='navbar-burger self-center xl:hidden'>
                 <svg width={35} height={35} viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg' >
                   <rect className='text-coolGray-50' width={32} height={32} rx={6} fill='currentColor' />
-                  <path
-                    className='text-coolGray-500'
+                  <path className='text-coolGray-500'
                     d='M7 12H25C25.2652 12 25.5196 11.8946 25.7071 11.7071C25.8946 11.5196 26 11.2652 26 11C26 10.7348 25.8946 10.4804 25.7071 10.2929C25.5196 10.1054 25.2652 10 25 10H7C6.73478 10 6.48043 10.1054 6.29289 10.2929C6.10536 10.4804 6 10.7348 6 11C6 11.2652 6.10536 11.5196 6.29289 11.7071C6.48043 11.8946 6.73478 12 7 12ZM25 15H7C6.73478 15 6.48043 15.1054 6.29289 15.2929C6.10536 15.4804 6 15.7348 6 16C6 16.2652 6.10536 16.5196 6.29289 16.7071C6.48043 16.8946 6.73478 17 7 17H25C25.2652 17 25.5196 16.8946 25.7071 16.7071C25.8946 16.5196 26 16.2652 26 16C26 15.7348 25.8946 15.4804 25.7071 15.2929C25.5196 15.1054 25.2652 15 25 15ZM25 20H7C6.73478 20 6.48043 20.1054 6.29289 20.2929C6.10536 20.4804 6 20.7348 6 21C6 21.2652 6.10536 21.5196 6.29289 21.7071C6.48043 21.8946 6.73478 22 7 22H25C25.2652 22 25.5196 21.8946 25.7071 21.7071C25.8946 21.5196 26 21.2652 26 21C26 20.7348 25.8946 20.4804 25.7071 20.2929C25.5196 20.1054 25.2652 20 25 20Z'
-                    fill='currentColor'
-                  />
+                    fill='currentColor' />
                 </svg>
               </button>
             </nav>
@@ -77,10 +71,7 @@ export default function Index() {
                 <nav className='relative p-6 h-full overflow-y-auto'>
                   <div className='flex flex-col justify-between h-full'>
                     <a className='inline-block' href='#'>
-                      <img
-                        className='h-8'
-                        src='flex-ui-assets/logos/flex-ui-green-light.svg'
-                        alt=''
+                      <img className='h-8' src='flex-ui-assets/logos/flex-ui-green-light.svg' alt=''
                       />
                     </a>
                     <ul className='py-6'>
@@ -138,7 +129,7 @@ export default function Index() {
                   <div className='flex flex-wrap'>
                     <div className='w-full md:w-auto py-1 md:py-0 md:mr-4'>
                       <a className='inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-green-50 font-medium text-center bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-green-500 rounded-md shadow-sm' href='#contact'>
-                        We're here to
+                        We're here to help
                       </a>
                     </div>
                   </div>
@@ -172,26 +163,14 @@ export default function Index() {
             </div>
           </div>
         </section>
-        <section
-          className='py-24 md:py-32 bg-white overflow-hidden' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center' }}>
+        <section id="mission" className='py-24 md:py-32 bg-white overflow-hidden' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center' }}>
           <div className='container px-4 mx-auto'>
             <div className='flex flex-wrap lg:items-center -mx-4'>
               <div className='w-full md:w-1/2 px-4 mb-16 md:mb-0'>
                 <div className='relative mx-auto md:ml-0 max-w-max'>
-                  <img
-                    className='absolute z-10 -left-8 -top-8 w-28 md:w-auto text-yellow-400'
-                    src='flex-ui-assets/elements/circle3-yellow.svg'
-                    alt=''
-                  />
-                  <img
-                    className='absolute z-10 -right-7 -bottom-8 w-28 md:w-auto text-blue-500'
-                    src='flex-ui-assets/elements/dots3-blue.svg'
-                    alt=''
-                  />
-                  <img
-                    src='../flex-ui-assets/images/features/stock2.png'
-                    alt=''
-                  />
+                  <img className='absolute z-10 -left-8 -top-8 w-28 md:w-auto text-yellow-400' src='flex-ui-assets/elements/circle3-yellow.svg' alt='our mission at ogcovered' />
+                  <img className='absolute z-10 -right-7 -bottom-8 w-28 md:w-auto text-blue-500' src='flex-ui-assets/elements/dots3-blue.svg' alt='our mission at ogcovered' />
+                  <img className='rounded-3xl' src='imgs/ogcovered-mission.jpg' alt='our mission at ogcovered' />
                 </div>
               </div>
               <div className='w-full md:w-1/2 px-4'>
@@ -218,7 +197,7 @@ export default function Index() {
             </div>
           </div>
         </section>
-        <section className='py-24 bg-white' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center', }}>
+        <section id="becovered" className='py-24 bg-white' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center', }}>
           <div className='container px-4 mx-auto'>
             <div className='xl:max-w-4xl mb-12 mx-auto text-center'>
               <span className='inline-block py-px px-2 mb-4 text-xs leading-5 text-green-500 bg-green-100 font-medium uppercase rounded-full shadow-sm'>
@@ -306,7 +285,7 @@ export default function Index() {
             </div>
           </div>
         </section>
-        <section className='py-24 bg-white' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center', }}>
+        <section id="benefits" className='py-24 bg-white' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center', }}>
           <div className='container px-4 mx-auto'>
             <div className='max-w-5xl mx-auto text-center mb-16 md:mb-24'>
               <span className='inline-block py-px px-2 mb-4 text-xs leading-5 text-green-500 bg-green-100 font-medium uppercase rounded-full shadow-sm'>
@@ -413,7 +392,7 @@ export default function Index() {
             </div>
           </div>
         </section>
-        <section className='pt-24 bg-white' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center' }}>
+        <section id="qualify" className='pt-24 bg-white' style={{ backgroundImage: 'url("flex-ui-assets/elements/pattern-white.svg")', backgroundPosition: 'center' }}>
           <div className='container px-4 mx-auto'>
             <div className='max-w-4xl mb-16'>
               <span className='inline-block py-px px-2 mb-4 text-xs leading-5 text-green-500 bg-green-100 font-medium rounded-full shadow-sm'>
