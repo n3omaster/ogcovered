739ea95b11d1bf7709e66a613902a5b45adfb63b
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-09-10 11:33:37 -0500

    Video autoPlay behavior

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,6 +10,9 @@ const meta = {
 };
 
 export default function Index() {
+
+  const videoUrl = "/vids/ogcovered.mp4";
+
   return (
     <>
       <HelmetProvider>
@@ -113,7 +116,7 @@ export default function Index() {
           <div className='py-20 md:py-28'>
             <div className='container px-4 mx-auto'>
               <div className='flex flex-wrap xl:items-center -mx-4'>
-                <div className='w-full md:w-1/2 px-4 mb-16 md:mb-0'>
+                <div className='w-full md:w-5/12 pr-2 mb-16 md:mb-0'>
                   <span className='inline-block py-px px-2 mb-4 text-xs leading-5 text-white bg-green-500 uppercase rounded-9xl'>
                     A better health for you
                   </span>
@@ -131,18 +134,14 @@ export default function Index() {
                     </div>
                   </div>
                 </div>
-                <div className='w-full md:w-1/2 px-4'>
+                <div className='w-full md:w-7/12 px-4'>
                   <div className='relative mx-auto md:mr-0 max-w-max'>
                     <img className='absolute z-10 -left-14 -top-12 w-28 md:w-auto' src='flex-ui-assets/elements/circle3-yellow.svg' alt='ogcovered' />
                     <img className='absolute z-10 -right-7 -bottom-8 w-28 md:w-auto' src='flex-ui-assets/elements/dots3-blue.svg' alt='ogcovered' />
-                    <svg className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 cursor-pointer text-green-500 hover:text-green-600' width={64} height={64} viewBox='0 0 64 64' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <circle cx={32} cy={32} r={32} fill='currentColor' />
-                      <path className='text-white' d='M40.5 31.13L26.5 23.05C26.348 22.9622 26.1755 22.916 26 22.916C25.8245 22.916 25.652 22.9622 25.5 23.05C25.3474 23.1381 25.2208 23.265 25.133 23.4177C25.0452 23.5705 24.9993 23.7438 25 23.92V40.08C24.9993 40.2562 25.0452 40.4295 25.133 40.5822C25.2208 40.735 25.3474 40.8619 25.5 40.95C25.652 41.0378 25.8245 41.084 26 41.084C26.1755 41.084 26.348 41.0378 26.5 40.95L40.5 32.87C40.6539 32.7828 40.7819 32.6563 40.871 32.5035C40.96 32.3506 41.007 32.1769 41.007 32C41.007 31.8231 40.96 31.6494 40.871 31.4965C40.7819 31.3437 40.6539 31.2172 40.5 31.13ZM27 38.35V25.65L38 32L27 38.35Z' fill='currentColor' />
-                    </svg>
                     <div className='relative overflow-hidden rounded-7xl'>
-                      <img src='/imgs/ogcovered-health-broker.jpg' />
-                      <video className='absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 min-h-full min-w-full max-w-none' poster='/imgs/ogcovered-health-broker.jpg' muted='' >
-                        <source src='/vids/ogcovered.mp4' type='video/mp4' />
+                      <video controls width="100%" className='' poster='/imgs/ogcovered-health-broker.jpg' loop muted autoPlay playsinline>
+                        <source src={videoUrl} type="video/mp4" />
+                        Sorry, your browser doesn't support videos.
                       </video>
                     </div>
                   </div>
